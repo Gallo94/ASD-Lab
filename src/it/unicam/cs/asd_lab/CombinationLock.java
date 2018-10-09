f79a11c3e980fba644444b6a41d20a3c92deadf3
Gallo94 <gallucciluca94@gmail.com>
2018-10-10 00:16:43 +0200

    starter

---
+++ src/it/unicam/cs/asd_lab/CombinationLock.java
@@ -0,0 +1,38 @@
+package it.unicam.cs.asd_lab;
+
+public class CombinationLock
+{
+	// Stato della cassaforte, true se open altrimenti false
+	private boolean open;
+	// contiene la combinazione
+	private char [] combination;
+	
+	// Prova ad aprire la cassaforte
+	public void open()
+	{
+		// TODO
+		return;
+	}
+	
+	/**
+	 * Ritorna lo stato della cassaforte
+	 * 
+	 * @return lo stato della cassaforte, true se aperta, false se chiusa
+	 */
+	public boolean isOpen()
+	{
+		// TODO
+		return false;
+	}
+	
+	/**
+	 * Gira la manopola in una posizione con una certa lettera
+	 * 
+	 * @param value la lettura su cui la manopola è stata postas
+	 */
+	public void setValue(char value)
+	{
+		// TODO
+		return;
+	}
+}
